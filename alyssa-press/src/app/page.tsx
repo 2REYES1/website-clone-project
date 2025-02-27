215a8444da0aad8da2dbdc3cc1c9c7ea5e1d6693
Alyssa <alyssalareyes@gmail.com>
2025-02-26 23:54:03 -0800

    attempted to recreate the app with an updated version

---
+++ alyssa-press/src/app/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Sidebar from "@/components/Sidebar";
+import Book from "@/components/Book";
+
+export default function Home() {
+  const books = Array.from({ length: 10 }, (_, index) => ({
+    spineImg: `/assets/book-spine-${index + 1}.png`,
+    coverImg: `/assets/book-cover-${index + 1}.png`,
+    descriptionTxt: `Book ${index + 1}`,
+  }));
+
+  return (
+    <div className="flex">
+      {/* Sticky Sidebar */}
+      <Sidebar books={books} />
+
+      {/* Content */}
+      <div className="flex-1 max-w-screen-lg mx-auto p-8">
+        <div className="flex flex-col items-center gap-8 mt-20 mb-12">
+          {books.map((book, index) => (
+            <div key={index}>
+              <Book spineImg={book.spineImg} coverImg={book.coverImg} descriptionTxt={book.descriptionTxt} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
